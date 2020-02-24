560e413649d0c50a9aa038ac9d6064d2087e979f
Danielius Visockas <danieliusvisockas@gmail.com>
2020-02-24 12:08:42 +0200

    Sort by last modified

--- docs/assets/js/fileUpload.js
+++ docs/assets/js/fileUpload.js
@@ -32,32 +32,30 @@ $(document).ready(function() {
     const compressionValue = parseInt($('#compression').val())
     const compression = compressionLevels[compressionValue]
 
-    // doc.text(35, 25, 'Hello, Manel :)')
     const images = $('.listItem > img')
     images.each(function(i, e) {
       let lastImage = i % 2
-      
+
       if (!lastImage) {
         doc.addImage(HEADER_IMAGE, 'JPEG', 0, 0, 42, 20, 'HEADER', compression)
       }
-      
+
       const ratio = $(e).height() / $(e).width()
       const imageSrc = $(e).attr('src')
-      
-      y = lastImage ? 155 : 25
-      const width = 200
-      
-      // addImage(imageData, format, x, y, width, height, alias, compression, rotation)
+
+      y = lastImage ? 145 : 25
+      const width = 190
+
       let imageType = imageSrc.match(/png;base64/) ? 'PNG' : 'JPG'
-      doc.addImage(imageSrc, imageType, 5, y, width, width * ratio, null, compression)
+      doc.addImage(imageSrc, imageType, 10, y, width, width * ratio, null, compression)
 
       if (lastImage || (i == (images.length - 1))) {
         doc.fromHTML('<b><i>CARDIOcare</i></b>', 10, 275)
-        // doc.text('', 10, 270)
-        // doc.setFont(FONT, 'bold')
+
         doc.setFontSize(9)
         doc.setFont(FONT, 'normal')
         doc.text('Serviço ambulatório de Cardiologia Veterinária', 11, 285)
+
         doc.addImage(FOOTER_IMAGE, 'JPEG', 80, 275, 37, 17, 'FOOTER', compression)
       }
 
@@ -86,23 +84,32 @@ $(document).ready(function() {
 
     for (let i = 0; i < files.length; i++) {
       let fileReader = new FileReader()
-      fileReader.onload = function(reader) {
-        addFile(reader.currentTarget.result)
-      }
+      fileReader.onloadend = (function (file) {
+        return function (reader) {
+          addFile(reader.currentTarget.result, file.lastModified)
+        }
+      })(files[i]);
       fileReader.readAsDataURL(files[i])
     }
   }
 
-  function addFile(uri) {
+  function addFile(uri, lastModified) {
     const fileList = $('#fileList')
-    let wrapper = $(document.createElement('div'))
+    let div = document.createElement('div')
+    div.dataset.lastModified = lastModified
+    let wrapper = $(div)
     wrapper.addClass('listItem')
     let img = $(document.createElement('img'))
     img.attr('src', uri)
     wrapper.append(img)
     fileList.append(wrapper)
+    wrapper.data('lastModified', 'lasasassa')
     window.fileCount -= 1
     if (window.fileCount == 0) {
+      fileList.find('.listItem').sort(function (a, b) {
+        return parseInt(b.dataset.lastModified) - parseInt(a.dataset.lastModified)
+      }).appendTo(fileList)
+
       fileList.sortable({
         placeholder: "sortablePlaceholder"
       })
@@ -112,11 +119,11 @@ $(document).ready(function() {
   function getDataUrl(img) {
     var canvas = document.createElement('canvas')
     var ctx = canvas.getContext('2d')
-  
+
     canvas.width = img.width
     canvas.height = img.height
     ctx.drawImage(img, 0, 0)
-  
+
     return canvas.toDataURL()
   }
 })
